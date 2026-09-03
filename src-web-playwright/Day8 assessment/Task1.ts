a868e6ccf38c4366c5770e67d8d76ab767ba3598
Kavipriyaa2308 <kavisaravanan238@gmail.com>
2026-09-03 15:46:29 +0530

    Updated Day8 Playwright

--- src-web-playwright/Day8 assessment/Task1.ts
+++ src-web-playwright/Day8 assessment/Task1.ts
@@ -9,21 +9,23 @@ await page.goto("https://www.medibuddy.in/")
 
 //Click login
 await page.locator("xpath=//a[text()='Login']").click();
-//Click I have a corporate Acccount
-await page.locator("xpath=//div[@ng-click='linkAccount()']").click();
-//await page.locator("xpath=//div[text()='I have a Corporate Account']").click();
+//Click I have a corporate Acccount (Use both text and attribute type both are working)
+//await page.locator("xpath=//div[@ng-click='linkAccount()']").click();
+await page.locator("xpath=//div[text()='I have a Corporate Account']").click();
 //Click Learn more
-await page.locator("xpath=//a[@ng-click='openLearnMoreSection()']").click();
-//await page.locator("xpath=//a[text()='Learn More']").click();
+//await page.locator("xpath=//a[@ng-click='openLearnMoreSection()']").click();
+await page.locator("xpath=//a[text()='Learn More']").click();
 //Click Skip
-await page.locator("xpath=//a[@ng-click='skipCorpUserPhone()']").click();
-//await page.locator("xpath=//a[text()='skip']").click();
+//await page.locator("xpath=//a[@ng-click='skipCorpUserPhone()']").click();
+await page.locator("xpath=//a[text()='skip']").click();
 
 //Click Login using username nd password
-await page.locator("xpath=//a[@ng-click='openUsernameScreen()']").click();
+//await page.locator("xpath=//a[@ng-click='openUsernameScreen()']").click();
+await page.locator("xpath=//a[text()='Login using Username & Password']").click();
 //enter username
 await page.locator("xpath=//input[@id='username']").fill("John")
-await page.locator("xpath=//button[text()='Proceed']").click();
+await page.locator("xpath=//button[text()='Proceed']").click(); //Orelse use normalize because proceed has some space bfr and after
+
 //enter password
 await page.locator("xpath=//input[@id='password']").fill("john123")
 
