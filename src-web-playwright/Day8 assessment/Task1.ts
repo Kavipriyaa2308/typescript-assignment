324c6600faf72d4c62ff23c790babb857256f9a6
Kavipriyaa2308 <kavisaravanan238@gmail.com>
2026-09-03 08:28:17 +0530

    Day8 playwright Task

---
+++ src-web-playwright/Day8 assessment/Task1.ts
@@ -0,0 +1,40 @@
+import { chromium } from "playwright";
+ 
+let browser=await chromium.launch({channel:"chrome",headless:false})
+let context=await browser.newContext()
+let page=await context.newPage()
+ 
+//navigate to URL
+await page.goto("https://www.medibuddy.in/")
+
+//Click login
+await page.locator("xpath=//a[text()='Login']").click();
+//Click I have a corporate Acccount
+await page.locator("xpath=//div[@ng-click='linkAccount()']").click();
+//await page.locator("xpath=//div[text()='I have a Corporate Account']").click();
+//Click Learn more
+await page.locator("xpath=//a[@ng-click='openLearnMoreSection()']").click();
+//await page.locator("xpath=//a[text()='Learn More']").click();
+//Click Skip
+await page.locator("xpath=//a[@ng-click='skipCorpUserPhone()']").click();
+//await page.locator("xpath=//a[text()='skip']").click();
+
+//Click Login using username nd password
+await page.locator("xpath=//a[@ng-click='openUsernameScreen()']").click();
+//enter username
+await page.locator("xpath=//input[@id='username']").fill("John")
+await page.locator("xpath=//button[text()='Proceed']").click();
+//enter password
+await page.locator("xpath=//input[@id='password']").fill("john123")
+
+//Click show password
+await page.locator("xpath=//img[@alt='hide-password']").click()
+//Click Signin
+await page.locator("xpath=//button[text()='Sign In']").click()
+//Get the error message and print
+const errorMessage= await page.locator("xpath=//div[@ng-if='isPasswordWrong']")
+console.log(await errorMessage.innerText());
+
+ 
+await page.waitForTimeout(10000)
+await browser.close()
