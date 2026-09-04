22b09e4422a699e56105f59bd6aafe2a8741e51b
Kavipriyaa2308 <kavisaravanan238@gmail.com>
2026-09-04 10:43:02 +0530

    Day9 Task

---
+++ src-web-playwright/Day9 assessment/Task1.ts
@@ -0,0 +1,33 @@
+import { chromium } from "playwright";
+ 
+let browser=await chromium.launch({channel:"chrome",headless:false})
+let context=await browser.newContext()
+let page=await context.newPage()
+ 
+//navigate to URL
+
+await page.goto("https://www.online.citibank.co.in/")
+ 
+//Accept cookies
+await page.locator("xpath=//button[@id='onetrust-accept-btn-handler']").click();
+ 
+await page.locator('text=My Account').hover();
+const [newPage,]=await Promise.all([context.waitForEvent("page"), page.locator("xpath=//div[text()='Banking with Citi']").click()])
+ 
+await newPage.waitForLoadState();
+ 
+console.log(await newPage.title());
+ 
+await newPage.locator("xpath=//input[@id='username']").fill("john123");
+await newPage.locator("xpath=//button[@id='signInBtn']").click();
+ 
+let errorMsg = await newPage.locator("xpath=//span[@role='alert']").innerText();
+console.log("Password Error:", errorMsg);
+
+
+await page.waitForTimeout(2000);
+await browser.close()
+
+
+
+
