83b6fca4790017bc82fada15b0f465b22b172cef
Kavipriyaa2308 <kavisaravanan238@gmail.com>
2026-09-04 12:47:25 +0530

    Updated Task1

--- src-web-playwright/Day9 assessment/Task1.ts
+++ src-web-playwright/Day9 assessment/Task1.ts
@@ -11,12 +11,12 @@ await page.goto("https://www.online.citibank.co.in/")
 //Accept cookies
 await page.locator("xpath=//button[@id='onetrust-accept-btn-handler']").click();
  
-await page.locator('text=My Account').hover();
-const [newPage,]=await Promise.all([context.waitForEvent("page"), page.locator("xpath=//div[text()='Banking with Citi']").click()])
+await page.locator("xpath=//div[text()='My Account']").hover();
+const [newPage]=await Promise.all([context.waitForEvent("page"), page.locator("xpath=//div[text()='Banking with Citi']").click()])
  
-await newPage.waitForLoadState();
+//await newPage.waitForLoadState();
  
-console.log(await newPage.title());
+//console.log(await newPage.title());
  
 await newPage.locator("xpath=//input[@id='username']").fill("john123");
 await newPage.locator("xpath=//button[@id='signInBtn']").click();
