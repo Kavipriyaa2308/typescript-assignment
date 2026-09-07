c3c88b3148a885d92ab7d7543ec1024aa8a5b152
Kavipriyaa2308 <kavisaravanan238@gmail.com>
2026-09-07 09:46:42 +0530

    Task2  Day10 updated

--- src-web-playwright/Day10 assessment/Task2.ts
+++ src-web-playwright/Day10 assessment/Task2.ts
@@ -17,9 +17,12 @@ await page.locator("xpath=//input[@name='firstName']").fill("John");
 await page.locator("xpath=//input[@name='middleName']").fill("A");
 await page.locator("xpath=//input[@name='lastName']").fill("Wick");
 //let employeeId = Math.floor(10000+Math.random()*90000).toString();
-//await page.locator("xpath=//label[text()='Employee Id']/following::input[1]").fill(employeeId);
+
 let employeeid="12345";
-await page.locator("xpath=//input[text()='Employee Id']").fill(employeeid);
+//await page.locator("xpath=//input[class()='oxd-input oxd-input--active']").fill(employeeId);
+
+await page.locator("xpath=//label[text()='Employee Id']").fill(employeeid);
+
 await page.locator("xpath=//input[@type='file']").setInputFiles("c:\\Users\\Administrator\\Downloads\\image1.jpg")
 
 await page.locator("xpath=//button[@type='submit']").click();
